1fb9b5888c6e683f5a53d9154d55f289b3978d0a
Patrick Mason <patrickmason73@gmail.com>
2023-01-31 17:44:44 -0700

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function eyesOnMeHandlerBlur() {
+        console.log('Hey! Eyes on me!')
+    }
+    function eyesOnMeHandlerFocus() {
+        console.log('Good!')
+    }
+    return (
+        <div>
+            <button onBlur={eyesOnMeHandlerBlur} onFocus={eyesOnMeHandlerFocus}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+function keypadHandler() {
+    console.log('Entering password...')
+}
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={keypadHandler}></input>
+        </div>
     )
 }
 
